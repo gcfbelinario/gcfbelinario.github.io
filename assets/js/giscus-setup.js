87144ca47dc97ff07324b5d3efdce79e5b75e5c6
gcfbelinario <gcfbelinario@gmail.com>
2026-01-01 18:08:59 +0000

    Deploying to gh-pages from @ gcfbelinario/gcfbelinario.github.io@9bfc190ff39bdf4cd0b43a30462f5061f0fecdbc 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gcfbelinario/gcfbelinario-webpage",
+    "data-repo": "gcfbelinario/gcfbelinario.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
